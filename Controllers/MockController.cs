49230e97cb98d2a5440abb1c6c786f50ec911ee3
weibaohui <weibaohui@yeah.net>
2024-05-05 16:23:35 +0800

    add linkpage options api

--- Controllers/MockController.cs
+++ Controllers/MockController.cs
@@ -273,4 +273,48 @@ public class MockController : Controller
             }
         };
     }
+
+
+    [HttpGet("Options/Level2")]
+    public object OptionsLevel2(string a)
+    {
+        var para = $"{a}";
+        var data = new
+        {
+            status = 0,
+            msg = "ok",
+            data = new
+            {
+                options = new[]
+                {
+                    new { label = $"选项{para}_a", value = $"{para}_a" },
+                    new { label = $"选项{para}_b", value = $"{para}_b" },
+                    new { label = $"选项{para}_c", value = $"{para}_c" }
+                }
+            }
+        };
+
+        return data;
+    }
+    [HttpGet("Options/Level3")]
+    public object OptionsLevel3(string b)
+    {
+        var para = $"{b}";
+        var data = new
+        {
+            status = 0,
+            msg = "ok",
+            data = new
+            {
+                options = new[]
+                {
+                    new { label = $"选项{para}_x", value = $"{para}_x" },
+                    new { label = $"选项{para}_y", value = $"{para}_y" },
+                    new { label = $"选项{para}_z", value = $"{para}_z" }
+                }
+            }
+        };
+
+        return data;
+    }
 }
