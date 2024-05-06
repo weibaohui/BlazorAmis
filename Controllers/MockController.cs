ce24f91501d17fb4ae1f4466ed5e1fe1bb04b55c
weibaohui <weibaohui@yeah.net>
2024-05-06 11:32:19 +0800

    update api

--- Controllers/MockController.cs
+++ Controllers/MockController.cs
@@ -8,6 +8,22 @@ public class MockController : Controller
 {
 
 
+    [HttpGet("Number/Random")]
+    public object RandomNumber()
+    {
+
+        var data = new
+        {
+            status = 0,
+            msg = "随机返回一个数字",
+            data = new
+            {
+                random =Enumerable.Range(0,10).MinBy(x => Guid.NewGuid())
+            }
+        };
+
+        return data;
+    }
 
 
 
